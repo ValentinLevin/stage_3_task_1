9346079ddd471b8af9f324338b7078b0eb180132
Valentin Levin <levin_valentin@mail.ru>
2024-05-14 21:19:32 +0500

    work 2024.05.14

--- module-repository/src/main/java/com/mjc/school/exception/DataFileNotFoundException.java
+++ module-repository/src/main/java/com/mjc/school/exception/DataFileNotFoundException.java
@@ -3,7 +3,12 @@ package com.mjc.school.exception;
 import com.mjc.school.exception.repository.CustomException;
 import com.mjc.school.exception.repository.ERROR_CODE;
 
+import java.io.Serial;
+
 public class DataFileNotFoundException extends CustomException {
+    @Serial
+    private static final long serialVersionUID = 2373539337608624346L;
+
     private static final String EXCEPTION_MESSAGE_TEMPLATE = "Not found data file with name %s";
 
     public DataFileNotFoundException(String filename) {
